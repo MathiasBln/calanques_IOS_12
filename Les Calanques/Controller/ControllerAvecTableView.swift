096e5bb3307d969ffb7eac2a59d96f26e24b54a9
MathiasBln <mlj.bouillon@outlook.fr>
2020-05-19 14:35:28 +0200

    Logique UITable view delegate

--- Les Calanques/Controller/ControllerAvecTableView.swift
+++ Les Calanques/Controller/ControllerAvecTableView.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ControllerAvecTableView: UIViewController {
+class ControllerAvecTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var calanques: [Calanque] = []
+    var cellID = "Alternative"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        calanques = CalanqueCollection().all()
         // Do any additional setup after loading the view.
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return calanques.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let calanque = calanques[indexPath.row]
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as? CalanqueCellAlternative {
+            cell.calanque = calanque
+            return cell
+        }
+        return UITableViewCell()
+    
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
---
+++ Les Calanques/View/CalanqueCellAlternative.swift
@@ -0,0 +1,39 @@
+//
+//  CalanqueCellAlternative.swift
+//  Les Calanques
+//
+//  Created by Mathias Bouillon on 19/05/2020.
+//  Copyright © 2020 Mathias Bouillon. All rights reserved.
+//
+
+import UIKit
+
+class CalanqueCellAlternative: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var calanqueIV: UIImageView!
+    @IBOutlet weak var nomLabel: UILabel!
+    
+    var calanque: Calanque? {
+        didSet {
+            if calanque != nil {
+                calanqueIV.image = calanque!.image
+                nomLabel.text = calanque!.nom
+            }
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
