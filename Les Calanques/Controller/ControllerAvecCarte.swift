c5865a2a3fe585f4db08f4447a2e155eadfcfd8e
MathiasBln <mlj.bouillon@outlook.fr>
2020-05-19 15:35:02 +0200

    add map with 3 view

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class ControllerAvecCarte: UIViewController {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: mapView.mapType = MKMapType.standard
+        case 1 : mapView.mapType = .satellite
+        case 2: mapView.mapType = .hybrid
+        default: break
+        }
+    }
+    @IBAction func getPosition(_ sender: Any) {
+    }
+    
     
 }
