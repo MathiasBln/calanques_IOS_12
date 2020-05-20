200dfbf6db0d4da4ebced95b9c890ca06ccea9b2
MathiasBln <mlj.bouillon@outlook.fr>
2020-05-20 11:59:44 +0200

    add annotation on map and use Notification to switch to an other page for detail

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -9,15 +9,80 @@
 import UIKit
 import MapKit
 
-class ControllerAvecCarte: UIViewController {
+class ControllerAvecCarte: UIViewController, MKMapViewDelegate {
 
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var calanques: [Calanque] = CalanqueCollection().all()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        addAnnotations()
+        NotificationCenter.default.addObserver(self, selector: #selector(notifDetail), name: Notification.Name("detail"), object: nil)
+    }
+    
+    
+    func toDetail(calanque: Calanque) {
+        performSegue(withIdentifier: "Detail", sender: calanque)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Detail"{
+            if let controller = segue.destination as? DetailController{
+                controller.calanqueRecue = sender as? Calanque
+            }
+        }
+    }
+    
+    func addAnnotations() {
+        for calanque in calanques {
+            
+            //annotation de base
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = calanque.coordonee
+//            annotation.title = calanque.nom
+//            mapView.addAnnotation(annotation)
+            
+            
+            //annotation custom
+            let annotation = MonAnnotation(calanque)
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "reuseID"
         
+        //Vérifier que ce ne soit pas l'utilisateur
+        if annotation.isKind(of: MKUserLocation.self){
+            return nil
+        }
+        
+        if let anno = annotation as? MonAnnotation{
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+           
+            if annotationView == nil{
+                
+            //Override
+                //annotationView = MonAnnotationView(annotation: anno, reuseIdentifier: reuseIdentifier)
+                
+                annotationView = MonAnnotationView(controller: self, annotation: anno, reuseIdentifier: reuseIdentifier)
+                
+                
+            //Version classique
+//                annotationView = MKAnnotationView(annotation: anno, reuseIdentifier: reuseIdentifier)
+//                annotationView?.image = UIImage(named: "placeholder")
+//                annotationView?.canShowCallout = true
+                return annotationView
+            } else {
+                return annotationView
+            }
+        }
+        
+        return nil
     }
     
     
@@ -32,5 +97,12 @@ class ControllerAvecCarte: UIViewController {
     @IBAction func getPosition(_ sender: Any) {
     }
     
+    
+    @objc func notifDetail(notification: Notification) {
+        if let calanque = notification.object as? Calanque {
+            print("J'ai une calanque")
+            toDetail(calanque: calanque)
+        }
+    }
     
 }
---
+++ Les Calanques/View/MonAnnotationView.swift
@@ -0,0 +1,87 @@
+//
+//  MonAnnotationView.swift
+//  Les Calanques
+//
+//  Created by Mathias Bouillon on 20/05/2020.
+//  Copyright © 2020 Mathias Bouillon. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MonAnnotationView: MKAnnotationView {
+    
+    var controller: ControllerAvecCarte?
+    
+    //initialiser le controlleur pour utiliser un Segue
+    init(controller: ControllerAvecCarte,annotation: MKAnnotation?, reuseIdentifier: String?) {
+        self.controller = controller
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        setupAnnotation()
+    }
+
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        setupAnnotation()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupAnnotation()
+    }
+    
+    func setupAnnotation() {
+        image = UIImage(named:"placeholder")
+        canShowCallout = true
+        
+        
+        //vue accesoire qu'on rajoute dans le pop-up
+        leftCalloutAccessoryView = setupLeft()
+        rightCalloutAccessoryView = setupRight()
+        detailCalloutAccessoryView = setupCenter()
+    }
+    
+    func setupLeft() -> UIButton{
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(UIImage(named: "distance"), for: .normal)
+        button.addTarget(self, action: #selector(gps), for: .touchUpInside)
+        return button
+    }
+    
+    func setupRight() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(UIImage(named: "detail"), for: .normal)
+        button.addTarget(self, action: #selector(detail), for: .touchUpInside)
+        return button
+    }
+    
+    func setupCenter() -> UIView? {
+        guard let anno = annotation as? MonAnnotation else { return nil }
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 125, height: 125))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 125).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 125).isActive = true
+        
+        let imageView = UIImageView(frame: view.bounds)
+        imageView.image = anno.calanque.image
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        view.addSubview(imageView)
+        
+        return view
+    }
+    
+    
+    @objc func detail(){
+        guard let anno = annotation as? MonAnnotation else { return }
+        
+        //controller?.toDetail(calanque: anno.calanque)
+        
+        NotificationCenter.default.post(name: Notification.Name("detail"), object: anno.calanque)
+    }
+    
+    @objc func gps(){
+        
+    }
+    
+}
